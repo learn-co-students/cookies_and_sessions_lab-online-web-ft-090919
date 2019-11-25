8fa6b5541fe00e365de871b7da67c3ddbd21f3ce
Jendnice <Pipeline38@hotmail.com>
2019-11-25 14:24:02 -0500

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,13 @@
+class ProductsController < ApplicationController
+  
+    def index
+    end 
+
+    def add 
+        cart << params[:product]
+        session[:cart] = cart 
+
+        redirect_to '/'
+    end 
+
+end
