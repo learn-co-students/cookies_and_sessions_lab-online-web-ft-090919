f42edd0bf893a1164babeb51eacf2e448cf12c3f
yasmine-hj <yasminehajaji95@gmail.com>
2019-11-27 12:22:36 +0000

    Done.

---
+++ app/controllers/products_controller.rb
@@ -0,0 +1,10 @@
+class ProductsController < ApplicationController
+    def index
+        @cart = cart
+    end
+
+    def create
+        cart << params[:item]
+        redirect_to products_path
+    end
+end 
